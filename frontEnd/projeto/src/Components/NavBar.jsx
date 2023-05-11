ce082914bdd64095affd4a3299529996b4cf4241
catarina <catarina.oliveira.41@hotmail.com>
2023-05-11 21:49:39 +0100

    home page done (sem pedidos ainda)

--- frontEnd/projeto/src/Components/NavBar.jsx
+++ frontEnd/projeto/src/Components/NavBar.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react"
 
 import Logo from "../Images/logo.png"
 import Info from "../Images/info.png"
@@ -6,21 +7,69 @@ import "../Styles/NavBar.css"
 
 export default function NavBar(){
 
+    const [showPopup, setShowPopup] = useState("none")
+
+    function showNone(){
+        setShowPopup("none")
+    }
+
+    function showLogin(event){
+        event.preventDefault();
+        setShowPopup("login")
+    }
+
+    function showSignup(){
+        setShowPopup("signup")
+    }
+
     return(
 
-        <nav className="navBar">
-            
-            <a href=""><img src={Logo} className="logoImage" alt="" /></a>
-
-            <div className="navBarRighSide">
-
-                <a href="/Home" className="infoTab"><img src={Info} alt="" /></a>
-                <a href="" className="loginTab">Login</a>
-                <a href="" className="sellTab">Sell Tickets!</a>
+        <div>
+            <nav className="navBar">
                 
-            </div>
+                <a href="/"><img className="logoImage" onClick={showNone} src={Logo} alt="" /></a>
 
-        </nav>
+                <div className="navBarRighSide">
+
+                    <a href="" className="infoTab"><img src={Info} alt="" /></a>
+                    <a href="" className="loginTab" onClick={showLogin}>Login</a>
+                    <a href="" className="sellTab">Sell Tickets!</a>
+                    
+                </div>
+
+            </nav>
+
+            {showPopup == "login" && (
+                <div className="popup">
+                    <div className="center">
+                        <div className="displayVertically">
+                            <p className="popupTitle">Login</p>
+                            <input className="input" type="text" placeholder="Insert username" />
+                            <input className="input" type="text" placeholder="Insert password" />
+                            <p className="popupLoginSignup" onClick={showSignup} >Don't have an account? Sign up here</p>
+                            <button className="popupButton" type="submit">Confirm</button>
+                        </div>
+                    </div>
+                </div>
+            )}
+
+
+            {showPopup == "signup" && (
+                <div className="popup">
+                    <div className="center">
+                        <div className="displayVertically">
+                            <p className="popupTitle">Sign up</p>
+                            <input className="input" type="text" placeholder="Insert email address" />
+                            <input className="input" type="text" placeholder="Insert username" />
+                            <input className="input" type="text" placeholder="Insert password" />
+                            <input className="input" type="text" placeholder="Confirm password" />
+                            <button className="popupButton" type="submit">Confirm</button>
+                        </div>
+                    </div>
+                </div>
+            )}
+
+        </div>
 
     )
 }
