4dbe8a6329d7c0cc1bf02fbff3d0930f798d7f41
obrunofilipe <brunofilipe377@gmail.com>
2023-06-13 15:48:32 +0100

    Spring setup for backend

---
+++ backEnd/AA-SIC/src/test/java/AASIC/AaSicApplicationTests.java
@@ -0,0 +1,13 @@
+package AASIC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AaSicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
