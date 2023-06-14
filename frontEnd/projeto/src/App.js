7a7ac4be9767e8e58be949bdeec719a35e519786
Francisco Toldy <chicomartinho8@gmail.com>
2023-06-14 13:24:36 +0100

    Interface 3 - Selling listings - versão estática

--- frontEnd/projeto/src/App.js
+++ frontEnd/projeto/src/App.js
@@ -1,23 +1,21 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import HomeUser from "./Pages/HomeUser"
+import HomeUser from "./Pages/HomeUser";
+import BoughtTickets from "./Pages/BoughtTickets";
 
-
-import "./Styles/General.css"
-import "./Styles/Event.css"
+import "./Styles/General.css";
+import "./Styles/Event.css";
+import BuyTicket from "./Pages/BuyTicket";
+import SellingListing from "./Pages/SellingListing";
 
 function App() {
   return (
-
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={
-          <HomeUser />
-        }/>
+        <Route path="/" element={<SellingListing />} />
       </Routes>
     </BrowserRouter>
-
   );
 }
 
---
+++ frontEnd/projeto/src/Pages/SellingListing.jsx
@@ -0,0 +1,107 @@
+import NavBarUser from "../Components/NavBar/NavBarUser";
+import Filters from "../Components/Filters/Filters";
+import ListingElem from "../Components/SellingListing/ListingElem";
+import PopUpSellingListing from "../Components/SellingListing/PopUpSellingListing";
+
+import { useState } from "react";
+
+export default function SellingListing() {
+  const [events, setEvents] = useState([
+    // para ser substituido pelo pedido com base no filtro
+    {
+      dayOfWeek: "Wednesday",
+      month: "May",
+      day: "8",
+      time: "9:25 PM",
+      ticketType: "Relvado",
+      eventName: "Coldplay - Music Of The Spheres World Tour",
+      eventPlace: "Estádio Cidade de Coimbra, Coimbra",
+      ticketPrice: "80.56",
+    },
+    {
+      dayOfWeek: "Friday",
+      month: "May",
+      day: "19",
+      time: "08:00 PM",
+      ticketType: "Floor",
+      eventName: "Post Malone - Twelve Carat Tour",
+      eventPlace: "Ziggo Dome, Amsterdam",
+      ticketPrice: "109.02",
+    },
+    {
+      dayOfWeek: "Thursday",
+      month: "Jul",
+      day: "7",
+      time: "03:00 PM",
+      ticketType: "1 Day Ticket | 7 July",
+      eventName: "NOS ALIVE'23",
+      eventPlace: "NOS Alive, Algés, Portugal",
+      ticketPrice: "91,07",
+    },
+    {
+      dayOfWeek: "Thursday",
+      month: "Jul",
+      day: "7",
+      time: "03:00 PM",
+      ticketType: "1 Day Ticket | 7 July",
+      eventName: "NOS ALIVE'23",
+      eventPlace: "NOS Alive, Algés, Portugal",
+      ticketPrice: "91,07",
+    },
+    {
+      dayOfWeek: "Thursday",
+      month: "Jul",
+      day: "7",
+      time: "03:00 PM",
+      ticketType: "1 Day Ticket | 7 July",
+      eventName: "NOS ALIVE'23",
+      eventPlace: "NOS Alive, Algés, Portugal",
+      ticketPrice: "91,07",
+    },
+    {
+      dayOfWeek: "Thursday",
+      month: "Jul",
+      day: "7",
+      time: "03:00 PM",
+      ticketType: "1 Day Ticket | 7 July",
+      eventName: "NOS ALIVE'23",
+      eventPlace: "NOS Alive, Algés, Portugal",
+      ticketPrice: "91,07",
+    },
+    {
+      dayOfWeek: "Thursday",
+      month: "Jul",
+      day: "7",
+      time: "03:00 PM",
+      ticketType: "1 Day Ticket | 7 July",
+      eventName: "NOS ALIVE'23",
+      eventPlace: "NOS Alive, Algés, Portugal",
+      ticketPrice: "91,07",
+    },
+  ]);
+  const [popUpTrigger, setPopUpTrigger] = useState(false);
+
+  const eventsFiltered = events.map((event) => {
+    return <ListingElem event={event} setPopUpTrigger={setPopUpTrigger} />;
+  });
+
+  return (
+    <div>
+      <NavBarUser selected="listings" />
+      <PopUpSellingListing
+        trigger={popUpTrigger}
+        setPopUpTrigger={setPopUpTrigger}
+      ></PopUpSellingListing>
+
+      <div className="center">
+        <div className="defaultContainer">
+          <h1>Tickets Listed</h1>
+
+          <Filters type="boughtTickets" />
+
+          <div className="eventsContainer">{eventsFiltered}</div>
+        </div>
+      </div>
+    </div>
+  );
+}
