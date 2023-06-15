f298830f1830fe34caeb675ec820473321fc57dd
Francisco Toldy <chicomartinho8@gmail.com>
2023-06-15 03:13:51 +0100

    Create Promoter Base

--- frontEnd/projeto/src/App.js
+++ frontEnd/projeto/src/App.js
@@ -7,14 +7,17 @@ import BoughtTickets from "./Pages/BoughtTickets";
 import "./Styles/General.css";
 import "./Styles/Event.css";
 import "./Styles/PaymentMethods.css";
+import "./Styles/CreatePromoter.css";
 
 import PaymentMethods from "./Pages/PaymentMethods";
+import Notifications from "./Pages/Notifications";
+import CreatePromoter from "./Pages/CreatePromoter";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<PaymentMethods />} />
+        <Route path="/" element={<CreatePromoter></CreatePromoter>} />
       </Routes>
     </BrowserRouter>
   );
--- frontEnd/projeto/src/Components/PaymentMethods/MBWayPopIn.jsx
+++ frontEnd/projeto/src/Components/PaymentMethods/MBWayPopIn.jsx
@@ -29,7 +29,6 @@ export default function MBWayPopIn({ setPaymentType }) {
               onKeyDown={handleKeyDown}
             />
           </label>
-          <input className="button" type="submit" value="Submit" />
         </form>
       </div>
     </div>
