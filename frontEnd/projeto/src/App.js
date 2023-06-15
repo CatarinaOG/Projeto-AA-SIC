18ad8daae327e652a6c5c3f3f0e421107ce76d4e
catarina <catarina.oliveira.41@hotmail.com>
2023-06-15 03:01:02 +0100

    imports

--- frontEnd/projeto/src/App.js
+++ frontEnd/projeto/src/App.js
@@ -1,22 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import HomeUser from "./Pages/HomeUser";
-import BoughtTickets from "./Pages/BoughtTickets";
 
-import "./Styles/General.css";
-import "./Styles/Event.css";
-import "./Styles/PaymentMethods.css";
+import BoughtTickets from "./Pages/BoughtTickets"
+import Browse from "./Pages/Browse"
+import Event from "./Pages/Event"
+import Home from "./Pages/Home"
+import HomeAdmin from "./Pages/HomeAdmin"
+import HomePromoter from "./Pages/HomePromoter"
+import HomeUser from "./Pages/HomeUser"
+import Notifications from "./Pages/Notifications"
+import PaymentMethods from "./Pages/PaymentMethods"
+import Profile from "./Pages/Profile"
+import SavedEvents from "./Pages/SavedEvents"
+import SellingListing from "./Pages/SellingListing"
+import SellTicket from "./Pages/SellTicket"
 
-import PaymentMethods from "./Pages/PaymentMethods";
+
+import "./Styles/General.css"
+import "./Styles/Event.css"
 
 function App() {
+
+  const [event,setEvent] = useState()
+
   return (
+
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<PaymentMethods />} />
       </Routes>
     </BrowserRouter>
+
   );
 }
 
