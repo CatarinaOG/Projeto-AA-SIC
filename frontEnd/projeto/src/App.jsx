569cd1db57dcd452b3447dbf184d7f7ab88d8dee
catarina <catarina.oliveira.41@hotmail.com>
2023-06-16 14:19:25 +0100

    suggest done

--- frontEnd/projeto/src/App.jsx
+++ frontEnd/projeto/src/App.jsx
@@ -2,44 +2,45 @@
 import React, { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import "./Styles/General.css";
-import "./Styles/Event.css";
-import "./Styles/PaymentMethods.css";
-import "./Styles/CreatePromoter.css";
-import "./Styles/EventsListing.css";
-import "./Styles/CreateEvent.css";
-import "./Styles/AddVenue.css";
 
-import PaymentMethods from "./Pages/PaymentMethods";
-import Notifications from "./Pages/Notifications";
-import CreatePromoter from "./Pages/CreatePromoter";
-import BoughtTickets from "./Pages/BoughtTickets";
-import Browse from "./Pages/Browse";
-import Event from "./Pages/Event";
-import Home from "./Pages/Home";
-import HomeAdmin from "./Pages/HomeAdmin";
-import HomePromoter from "./Pages/HomePromoter";
-import HomeUser from "./Pages/HomeUser";
-import Profile from "./Pages/Profile";
-import SavedEvents from "./Pages/SavedEvents";
-import SellingListing from "./Pages/SellingListing";
-import SellTicket from "./Pages/SellTicket";
-import EventsListing from "./Pages/EventsListing";
-import CreateEvent from "./Pages/CreateEvent";
-import AddVenue from "./Pages/AddVenue";
+import BoughtTickets from "./Pages/BoughtTickets"
+import Browse from "./Pages/Browse"
+import Event from "./Pages/Event"
+import Home from "./Pages/Home"
+import HomeAdmin from "./Pages/HomeAdmin"
+import HomePromoter from "./Pages/HomePromoter"
+import HomeUser from "./Pages/HomeUser"
+import Notifications from "./Pages/Notifications"
+import PaymentMethods from "./Pages/PaymentMethods"
+import Profile from "./Pages/Profile"
+import SavedEvents from "./Pages/SavedEvents"
+import SellingListing from "./Pages/SellingListing"
+import SellTicket from "./Pages/SellTicket"
 
-import "./Styles/General.css";
-import "./Styles/Event.css";
+
+import "./Styles/General.css"
+import "./Styles/Event.css"
+import SuggestEvent from "./Pages/SuggestEvent";
 
 function App() {
-  const [event, setEvent] = useState();
+
+  const [event,setEvent] = useState()
+  const [user,setUser] = useState({
+    name: "catarina",
+    id: 1,
+  })
+
 
   return (
+
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<AddVenue></AddVenue>} />
+        <Route path="/" element={
+          	<SuggestEvent />
+        }/>
       </Routes>
     </BrowserRouter>
+
   );
 }
 
