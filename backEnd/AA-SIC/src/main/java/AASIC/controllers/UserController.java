d3ab4c8184fc271c44b6ae15d9a5bc5162ad7908
obrunofilipe <brunofilipe377@gmail.com>
2023-06-16 02:44:40 +0100

    gitignore update

--- backEnd/AA-SIC/src/main/java/AASIC/controllers/UserController.java
+++ backEnd/AA-SIC/src/main/java/AASIC/controllers/UserController.java
@@ -1,20 +1,30 @@
 package AASIC.controllers;
 
 //import javax.annotation.Resource;
-import org.springframework.beans.factory.annotation.Autowired;
+import AASIC.services.AuthenticationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 //import org.json.*;
-import AASIC.model.*;
-import AASIC.repositories.*;
+
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping(path = "/api/user")
-public class UserController {
+public class AuthController {
 
+    private final AuthenticationService authService ;
+    @PostMapping("/register")
+    public ResponseEntity<AuthenticationResponse> register(@RequestBody RegisterRequest request){
+        return ResponseEntity.ok(authService.register(request));
+    }
 
-
+    @PostMapping("/register")
+    public ResponseEntity<AuthenticationResponse> login(@RequestBody AuthenticationRequest request){
+        return ResponseEntity.ok(authService.login(request));
+    }
 }
